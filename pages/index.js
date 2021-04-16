58ff45cf067930ef5749218f751caee13a3f55a1
zainsci <60652827+zainsci@users.noreply.github.com>
2021-04-16 21:32:32 +0500

    Setup Home Page

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,49 @@
 import Layout from "../components/layout";
 
 const Home = () => {
-  return <Layout>Home Page</Layout>;
+  return (
+    <Layout>
+      <div className="home">
+        <div className="home__left">
+          <div className="home__left__img">
+            <img src="/assets/images/me.jpg" alt="zainsci" />
+          </div>
+        </div>
+        <div className="home__right">
+          <p>Hello There!</p>
+          <h1>zainsci Here.</h1>
+          <p>
+            I am a self taught Web Developer and work mostly with Python, Flask,
+            JavaScript and React. Your can find me on{" "}
+            <a
+              href="https://www.twitter.com/zainsci"
+              target="_blank"
+              rel="nereferrer"
+            >
+              Twitter
+            </a>
+            ,{" "}
+            <a
+              href="https://www.github.com/zainsci"
+              target="_blank"
+              rel="nereferrer"
+            >
+              Github
+            </a>{" "}
+            and{" "}
+            <a
+              href="https://www.instagram.com/zainsci"
+              target="_blank"
+              rel="nereferrer"
+            >
+              Instagram
+            </a>
+            .
+          </p>
+        </div>
+      </div>
+    </Layout>
+  );
 };
 
 export default Home;
