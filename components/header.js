771f49e6d8eeb4b2d5fb7540057155abbc4f914c
zainsci <60652827+zainsci@users.noreply.github.com>
2021-04-17 15:02:13 +0500

    Remove About Page

--- components/header.js
+++ components/header.js
@@ -22,11 +22,6 @@ const Header = () => {
             </Link>
           </li>
           <li className="nav__item">
-            <Link href="/about">
-              <a className="nav__link">About</a>
-            </Link>
-          </li>
-          <li className="nav__item">
             <Link href="/contact">
               <a className="nav__link">Contact</a>
             </Link>
--- pages/about.js
+++
@@ -1,7 +0,0 @@
-import Layout from "../components/layout";
-
-const About = () => {
-  return <Layout>About Page</Layout>;
-};
-
-export default About;
