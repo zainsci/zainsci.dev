d736a852045f5fe367680cbf6bb89968d7b83a7f
zainsci <60652827+zainsci@users.noreply.github.com>
2021-04-17 15:38:24 +0500

    Add Initital Content to Contact Page

--- pages/contact.js
+++ pages/contact.js
@@ -1,7 +1,43 @@
 import Layout from "../components/layout";
 
 const Contact = () => {
-  return <Layout>Contact Page</Layout>;
+  return (
+    <Layout>
+      <div className="contact">
+        <p>
+          ____________{" "}
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="24"
+            height="24"
+            viewBox="0 0 24 24"
+            fill="none"
+            stroke="currentColor"
+            stroke-width="2"
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            class="feather feather-send"
+          >
+            <line x1="22" y1="2" x2="11" y2="13"></line>
+            <polygon points="22 2 15 22 11 13 2 9 22 2"></polygon>
+          </svg>{" "}
+          ____________
+        </p>
+        <p>
+          I don't look at the emails most of the time so if anyone wants to
+          contact me, they can do so by by messaging me on my{" "}
+          <a
+            href="https://twitter.com/zainsci"
+            target="_blank"
+            rel="noreferrer"
+          >
+            Twitter Account
+          </a>
+          .
+        </p>
+      </div>
+    </Layout>
+  );
 };
 
 export default Contact;
