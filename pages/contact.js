2c55072d8fe155c46c5f6f0b9d6f7886db1beac0
zainsci <60652827+zainsci@users.noreply.github.com>
2021-04-20 15:14:13 +0500

    Delete Some Attrs

--- pages/contact.js
+++ pages/contact.js
@@ -14,10 +14,8 @@ const Contact = () => {
             viewBox="0 0 24 24"
             fill="none"
             stroke="currentColor"
-            stroke-width="2"
-            stroke-linecap="round"
-            stroke-linejoin="round"
-            class="feather feather-send"
+            strokeWidth="2"
+            className="feather feather-send"
           >
             <line x1="22" y1="2" x2="11" y2="13"></line>
             <polygon points="22 2 15 22 11 13 2 9 22 2"></polygon>
