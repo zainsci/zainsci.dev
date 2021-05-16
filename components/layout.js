ebc53949c09831c524d80fdae2f8ee0677a07f7a
zainsci <60652827+zainsci@users.noreply.github.com>
2021-05-16 11:47:42 +0500

    feat: SEO and socail media meta tags

--- components/layout.js
+++ components/layout.js
@@ -28,6 +28,26 @@ const Layout = ({ children, title }) => {
         <title>{title} - zainsci</title>
         <link rel="icon" href="/favicon.ico" type="image/x-icon" />
         <link rel="manifest" href="/manifest.json" />
+
+        <meta charset="UTF-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <meta
+          name="description"
+          content="zainsci - Self-Taught Web Developer and Programmer"
+        />
+        <meta property="og:title" content="zainsci" />
+        <meta
+          property="og:description"
+          content="zainsci - Self-Taught Web Developer and Programmer"
+        />
+        <meta
+          property="og:image"
+          content="https://zainsci.github.io/potrait.jpg"
+        />
+        <meta property="og:url" content="https://zainsci.github.io" />
+        <meta property="og:site_name" content="zainsci" />
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta name="twitter:image:alt" content="zainsci's portrait" />
       </Head>
       <Header />
 
