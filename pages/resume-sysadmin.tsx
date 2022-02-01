2fd7ce30042df0046d41e92cbf5c8071abaca578
zainsci <60652827+zainsci@users.noreply.github.com>
2022-02-01 12:18:34 +0500

    feat: Add Resume Page and add data for IT Manager

---
+++ pages/resume-sysadmin.tsx
@@ -0,0 +1,329 @@
+const ResumeSysAdmin = () => {
+  return (
+    <main className="resume">
+      <div className="head">
+        <div className="portrait">
+          <img src="/assets/images/me.jpg" alt="zainsci" />
+        </div>
+        <div className="headings">
+          <h1>Zain Shabbir</h1>
+          <h3>IT Admin & Programmer</h3>
+          <p>
+            Results-driven passionate system administrator and Programmer,
+            seeking to use proven scripting and problem solving skills to
+            improve cost and performance. Created and managed Computer Networks
+            at a local Internet Cafe and Superior College Wazirabad.{" "}
+          </p>
+        </div>
+      </div>
+      <div className="contacts">
+        <ul>
+          <li>
+            <a href="email:zainansari08@gmail.com">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="24"
+                height="24"
+                viewBox="0 0 24 24"
+                fill="none"
+                stroke="currentColor"
+                stroke-width="2"
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                className="feather feather-mail"
+              >
+                <path d="M4 4h16c1.1 0 2 .9 2 2v12c0 1.1-.9 2-2 2H4c-1.1 0-2-.9-2-2V6c0-1.1.9-2 2-2z"></path>
+                <polyline points="22,6 12,13 2,6"></polyline>
+              </svg>{" "}
+              zainansari08@gmail.com
+            </a>
+          </li>
+          <li>
+            <a href="tel:+923034702074">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="24"
+                height="24"
+                viewBox="0 0 24 24"
+                fill="none"
+                stroke="currentColor"
+                stroke-width="2"
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                className="feather feather-smartphone"
+              >
+                <rect x="5" y="2" width="14" height="20" rx="2" ry="2"></rect>
+                <line x1="12" y1="18" x2="12.01" y2="18"></line>
+              </svg>{" "}
+              +92 303 470 2 074
+            </a>
+          </li>
+          <li>
+            <a href="">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="24"
+                height="24"
+                viewBox="0 0 24 24"
+                fill="none"
+                stroke="currentColor"
+                stroke-width="2"
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                className="feather feather-map-pin"
+              >
+                <path d="M21 10c0 7-9 13-9 13s-9-6-9-13a9 9 0 0 1 18 0z"></path>
+                <circle cx="12" cy="10" r="3"></circle>
+              </svg>{" "}
+              Ghakhar, Gujranwala
+            </a>
+          </li>
+          <li>
+            <a href="https://linkedin.com/in/zainsci" target="_blank">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="24"
+                height="24"
+                viewBox="0 0 24 24"
+                fill="none"
+                stroke="currentColor"
+                stroke-width="2"
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                className="feather feather-linkedin"
+              >
+                <path d="M16 8a6 6 0 0 1 6 6v7h-4v-7a2 2 0 0 0-2-2 2 2 0 0 0-2 2v7h-4v-7a6 6 0 0 1 6-6z"></path>
+                <rect x="2" y="9" width="4" height="12"></rect>
+                <circle cx="4" cy="4" r="2"></circle>
+              </svg>{" "}
+              linkedin.com/in/zainsci
+            </a>
+          </li>
+          <li>
+            <a href="https://zainsci.dev" target="_blank">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="24"
+                height="24"
+                viewBox="0 0 24 24"
+                fill="none"
+                stroke="currentColor"
+                stroke-width="2"
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                className="feather feather-home"
+              >
+                <path d="M3 9l9-7 9 7v11a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2z"></path>
+                <polyline points="9 22 9 12 15 12 15 22"></polyline>
+              </svg>
+              zainsci.dev
+            </a>
+          </li>
+          <li>
+            <a href="https://twitter.com/zainsci" target="_blank">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="24"
+                height="24"
+                viewBox="0 0 24 24"
+                fill="none"
+                stroke="currentColor"
+                stroke-width="2"
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                className="feather feather-twitter"
+              >
+                <path d="M23 3a10.9 10.9 0 0 1-3.14 1.53 4.48 4.48 0 0 0-7.86 3v1A10.66 10.66 0 0 1 3 4s-4 9 5 13a11.64 11.64 0 0 1-7 2c9 5 20 0 20-11.5a4.5 4.5 0 0 0-.08-.83A7.72 7.72 0 0 0 23 3z"></path>
+              </svg>
+              twitter.com/zainsci
+            </a>
+          </li>
+        </ul>
+      </div>
+      <div className="body">
+        <section className="left">
+          <div className="work">
+            <h2>Work Experience</h2>
+            <div>
+              <h3>Freelance Web Developer</h3>
+              <h4>Fiverr</h4>
+              <div className="work__status">
+                <span>Ghakhar</span>
+                <span>2021 - On Going</span>
+              </div>
+              <ul>
+                <li>
+                  Developed and maintained Large Scale Applications for clients
+                  from all over the world.
+                </li>
+                <li>
+                  Developed Cross-Browser compatible ceb applications with
+                  responsive design.
+                </li>
+                <li>
+                  Developed high performance Chrome extensions to extend browser
+                  capability.
+                </li>
+              </ul>
+            </div>
+            <div>
+              <h3>IT Manager</h3>
+              <h4>Shahzad & CO. Ghakhar</h4>
+              <div className="work__status">
+                <span>Ghakhar</span>
+                <span>2019 - On Going</span>
+              </div>
+              <ul>
+                <li>Manage and Maintain Computer Network for users.</li>
+                <li>
+                  Manage, maintain and backup company data on company serve on a
+                  daily baisis.
+                </li>
+                <li>
+                  Manage user data and connectivity to company in Saudia Arabia
+                </li>
+              </ul>
+            </div>
+          </div>
+          <div className="certs">
+            <h2>Certificates</h2>
+            <ul>
+              <li>
+                <strong>CS50</strong> - Introduction To Computer Science
+              </li>
+              <li>
+                <strong>CS50Web</strong> - Introduction to Web Programming
+              </li>
+              <li>
+                <strong>LinkedIn Certifications</strong> in Networking, Network
+                Security, Programming, and DataBases.
+              </li>
+            </ul>
+          </div>
+        </section>
+        <section className="right">
+          <div className="skills">
+            <h2>Skills And Competencies</h2>
+            <ul>
+              <li>Sysetm Admin</li>
+              <li>Network Troubleshooting</li>
+              <li>Ubuntu & Linux</li>
+              <li>Python</li>
+              <li>JavaScript</li>
+              <li>SQL</li>
+              <li>Programming</li>
+              <li>Database Management</li>
+              <li>Virtualization (VMWare & Virtual Box)</li>
+            </ul>
+          </div>
+
+          <div className="achievements">
+            <h2>Projects</h2>
+            <ul>
+              <li>
+                Database Management Application For Blood Groups Details for a
+                local NGO.
+              </li>
+              <li>System Tools for a local company.</li>
+              <li>Scripts to Backup Company & User Data on a daily basis.</li>
+              <li>
+                Implementing Practices to secure company data and prevent data
+                breach.
+              </li>
+            </ul>
+          </div>
+          <div className="langs">
+            <h2>Languages</h2>
+            <ul>
+              <li>Urdu</li>
+              <li>English</li>
+              <li>Punjabi</li>
+              <li>Japanese</li>
+              <li>Arabic</li>
+            </ul>
+          </div>
+          <div className="hobbies">
+            <h2>Interests & Hobbies</h2>
+            <ul>
+              <li>
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="24"
+                  height="24"
+                  viewBox="0 0 24 24"
+                  fill="none"
+                  stroke="currentColor"
+                  stroke-width="2"
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  className="feather feather-book"
+                >
+                  <path d="M4 19.5A2.5 2.5 0 0 1 6.5 17H20"></path>
+                  <path d="M6.5 2H20v20H6.5A2.5 2.5 0 0 1 4 19.5v-15A2.5 2.5 0 0 1 6.5 2z"></path>
+                </svg>
+                Reading
+              </li>
+
+              <li>
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="24"
+                  height="24"
+                  viewBox="0 0 24 24"
+                  fill="none"
+                  stroke="currentColor"
+                  stroke-width="2"
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  className="feather feather-code"
+                >
+                  <polyline points="16 18 22 12 16 6"></polyline>
+                  <polyline points="8 6 2 12 8 18"></polyline>
+                </svg>
+                Programming
+              </li>
+              <li>
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="24"
+                  height="24"
+                  viewBox="0 0 24 24"
+                  fill="none"
+                  stroke="currentColor"
+                  stroke-width="2"
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  className="feather feather-award"
+                >
+                  <circle cx="12" cy="8" r="7"></circle>
+                  <polyline points="8.21 13.89 7 23 12 20 17 23 15.79 13.88"></polyline>
+                </svg>
+                Gaming
+              </li>
+              <li>
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="24"
+                  height="24"
+                  viewBox="0 0 24 24"
+                  fill="none"
+                  stroke="currentColor"
+                  stroke-width="2"
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  className="feather feather-edit-3"
+                >
+                  <path d="M12 20h9"></path>
+                  <path d="M16.5 3.5a2.121 2.121 0 0 1 3 3L7 19l-4 1 1-4L16.5 3.5z"></path>
+                </svg>
+                Writing
+              </li>
+            </ul>
+          </div>
+        </section>
+      </div>
+    </main>
+  )
+}
+
+export default ResumeSysAdmin
